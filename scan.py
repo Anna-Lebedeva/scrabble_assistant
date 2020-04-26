8f309a57c8b43bb9c93f2af0db7c79d70d579edf
Pavel Chernov <64083367+RiordanFols@users.noreply.github.com>
2020-04-26 20:51:49 +0300

    Commit testing

--- scan.py
+++ scan.py
@@ -9,8 +9,7 @@ ratio = image.shape[0] / 750.0
 orig = image.copy()
 image = imutils.resize(image, height=750)
 
-# convert the image to grayscale, blur it, and find edges
-# in the image
+# grayscale, gaussian blur and edged
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 25, 170)
@@ -23,13 +22,14 @@ cv2.destroyAllWindows()
 
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
-# loop over the contours
 screenCnt = None
-for c in cnts:
+# loop over the contours
+for c in contours:
+    # approximation
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
@@ -45,13 +45,6 @@ cv2.destroyAllWindows()
 # apply the four point transform to obtain a top-down
 # view of the original image
 warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
-
-# convert the warped image to grayscale, then threshold it
-# to give it that 'black and white' paper effect
-
-# warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-# T = threshold_local(warped, 11, offset=10, method="gaussian")
-# warped = (warped > T).astype("uint8") * 255
 
 # show the original and scanned images
 cv2.imshow("Original", imutils.resize(orig, height=750))
