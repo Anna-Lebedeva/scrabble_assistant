60be623ce25559fc71a6acdffcb140f8748ac9dd
Pavel Chernov <64083367+RiordanFols@users.noreply.github.com>
2020-05-01 20:00:57 +0300

    external contour search + hint_search changes + new images
    Добавил функцию поиска внешнего контура доски (работает на загруженых примерах) и поправил hint_search

--- hint_search.py
+++ hint_search.py
@@ -11,6 +11,105 @@ def get_best_hint(board: [[str]], letters: dict) -> [[str]]:
     :return: символьный двумерный массив с буквами подсказки
     """
 
+
+def mark_blocked_cells(board: [[str]]) -> [[str]]:
+    """
+    Помечает клетки, в которых не может быть букв знаком '#'.
+    Если рядом с клеткой по вертикали и горизонтали есть хотя бы по фишке, клетка заблокирована
+    :param board: символьный двумерный массив доски
+    :return: тот же массив с помеченными заблокированными клетками
+    """
+
+    for y_index in range(15):
+        for x_index in range(15):
+            if board[y_index][x_index] == '':  # если клетка пуста
+                x_block = False  # заблокирована ли клетка по горизонтали
+                y_block = False  # заблокирована ли клетка по вертикали
+
+                if y_index > 0:  # если есть пространство сверху
+                    if board[y_index - 1][x_index] != '' and board[y_index - 1][x_index] != '#':
+                        y_block = True
+                if y_index < 14:  # если есть пространство снизу
+                    if board[y_index + 1][x_index] != '' and board[y_index + 1][x_index] != '#':
+                        y_block = True
+
+                if x_index > 0:  # если есть пространство слева
+                    if board[y_index][x_index - 1] != '' and board[y_index][x_index - 1] != '#':
+                        x_block = True
+                if x_index < 14:  # если есть пространство справа
+                    if board[y_index][x_index + 1] != '' and board[y_index][x_index + 1] != '#':
+                        x_block = True
+
+                if x_block and y_block:
+                    board[y_index][x_index] = '#'
+
+                # if x_index > 0 and y_index > 0:
+                #     if board[y_index][x_index - 1] and board[y_index - 1][x_index]:  # если есть слева-сверху
+                #         if board[y_index][x_index - 1] != '#' and board[y_index - 1][x_index] != '#':
+                #             board[y_index][x_index] = '#'
+                #             continue
+                #
+                # if x_index < 14 and y_index > 0:
+                #     if board[y_index][x_index + 1] and board[y_index - 1][x_index]:  # если есть справа-сверху
+                #         if board[y_index][x_index + 1] != '#' and board[y_index - 1][x_index] != '#':
+                #             board[y_index][x_index] = '#'
+                #             continue
+                #
+                # if y_index < 14 and x_index < 14:
+                #     if board[y_index + 1][x_index] and board[y_index][x_index + 1]:  # если есть снизу-справа
+                #         if board[y_index - 1][x_index] != '#' and board[y_index][x_index + 1] != '#':
+                #             board[y_index][x_index] = '#'
+                #             continue
+                #
+                # if y_index < 14 and x_index > 0:
+                #     if board[y_index + 1][x_index] and board[y_index][x_index - 1]:  # если есть снизу-слева
+                #         if board[y_index + 1][x_index] != '#' and board[y_index][x_index - 1] != '#':
+                #             board[y_index][x_index] = '#'
+    return board
+
+
+def transpose_board(board: [[str]]) -> [[str]]:
+    """
+    Искать слова удобнее в строке, а не в столбце. Переводит столбцы в строки.
+    :param board: символьный двумерный массив доски
+    :return: транспонированная матрица
+    """
+
+    return np.array(board).transpose()
+
+
+def is_word_fits(vector: [str], word: str, start_position: int, end_position: int) -> bool:
+    """
+    Проверяет подходит ли слово в линию
+    :param vector: символьный одномерный массив со специальными символами в заблокированных клетках
+    :param word: слово в виде строки
+    :param start_position: индекс позиции начала слова
+    :param end_position: индекс позиции конца слова
+    :return: true - слово подходит, false - слово не подходит
+    """
+
+
+def read_json(filename: str) -> dict:
+    """
+    Считывает json-файл в dict
+    :param filename: имя json-файла
+    :return: считанный словарь
+    """
+
+    with open(file=Path(Path.cwd() / filename), mode='r', encoding='utf-8') as file:
+        return dict(json.load(file))
+
+
+def calculate_word_value(word: str, json_filename: str, start_position: int, end_position: int) -> int:
+    """
+    Считает ценность слова
+    :param word: слово в виде строки
+    :param json_filename: имя json-словаря с ценностью букв
+    :param start_position: индекс позиции начала слова
+    :param end_position: индекс позиции конца слова
+    :return: ценность слова
+    """
+
     # разметка ценности полей доски:
     # 0 - обычное поле
     # 1 - х2 за букву
@@ -35,113 +134,6 @@ def get_best_hint(board: [[str]], letters: dict) -> [[str]]:
                     [4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4],
                     ]
 
-
-def mark_blocked_cells(board_letters: [[str]]) -> [[str]]:
-    """
-    Помечает клетки, в которых не может быть букв знаком '#'.
-
-    :param board_letters: принимает распознанную символьную сетку
-    :return: возвращает распознанную символьную сетку с помеченными заблокированными клетками
-    """
-
-    # пример распознаной доски, переданной на вход:
-    board_letters = [
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', 'т', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', 'о', '', '', '', '', ''],
-        ['', '', '', 'п', 'о', 'c', 'е', 'л', 'о', 'к', '', '', '', '', ''],
-        ['', '', '', 'а', '', 'а', '', '', '', '', '', 'р', '', '', ''],
-        ['', '', '', 'п', '', 'д', 'о', 'м', '', 'я', '', 'е', '', '', ''],
-        ['', '', '', 'а', '', '', '', 'а', 'з', 'б', 'у', 'к', 'а', '', ''],
-        ['', '', '', '', '', 'с', 'о', 'м', '', 'л', '', 'а', '', '', ''],
-        ['', '', '', 'я', 'м', 'а', '', 'а', '', 'о', '', '', '', '', ''],
-        ['', '', '', '', '', 'л', '', '', '', 'к', 'и', 'т', '', '', ''],
-        ['', '', '', '', 'с', 'о', 'л', 'ь', '', 'о', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-    ]
-
-    for ver_char in range(15):
-        for hor_char in range(15):
-            if not board_letters[ver_char][hor_char]:  # находит пустую клетку
-
-                if hor_char > 0 and ver_char > 0:
-                    if board_letters[ver_char][hor_char - 1] and \
-                            board_letters[ver_char - 1][hor_char]:  # если есть слева-сверху
-                        if board_letters[ver_char][hor_char - 1] != '#' and \
-                                board_letters[ver_char - 1][hor_char] != '#':
-                            board_letters[ver_char][hor_char] = '#'
-                            continue
-
-                if hor_char < 14 and ver_char > 0:
-                    if board_letters[ver_char][hor_char + 1] and \
-                            board_letters[ver_char - 1][hor_char]:  # если есть справа-сверху
-                        if board_letters[ver_char][hor_char + 1] != '#' and \
-                                board_letters[ver_char - 1][hor_char] != '#':
-                            board_letters[ver_char][hor_char] = '#'
-                            continue
-
-                if ver_char < 14 and hor_char < 14:
-                    if board_letters[ver_char + 1][hor_char] and \
-                            board_letters[ver_char][hor_char + 1]:  # если есть снизу-справа
-                        if board_letters[ver_char - 1][hor_char] != '#' and \
-                                board_letters[ver_char][hor_char + 1] != '#':
-                            board_letters[ver_char][hor_char] = '#'
-                            continue
-
-                if ver_char < 14 and hor_char > 0:
-                    if board_letters[ver_char + 1][hor_char] and \
-                            board_letters[ver_char][hor_char - 1]:  # если есть снизу-слева
-                        if board_letters[ver_char + 1][hor_char] != '#' and \
-                                board_letters[ver_char][hor_char - 1] != '#':
-                            board_letters[ver_char][hor_char] = '#'
-    return board_letters
-
-
-def get_horizontal_from_vertical(board: [[str]]) -> [[str]]:
-    """
-    Искать слова убоднее в строке, а не в столбце. Переводит столбцы в строки.
-
-    :param board: символьный двумерный массив доски
-    :return: массив вертикалей, переведенный в массив горизонталей (транспонированную матрицу)
-    """
-
-    return np.array(board).transpose()
-
-
-def is_word_fits(line: [str], word: str, start_position: int,
-                 end_position: int) -> bool:
-    """
-    Проверяет подходит ли слово в линию
-    :param line: символьный одномерный массив со специальными символами в заблокированных клетках
-    :param word: слово в виде строки
-    :param start_position: индекс позиции начала слова
-    :param end_position: индекс позиции конца слова
-    :return: true - слово подходит, false - слово не подходит
-    """
-
-
-def read_json(filename: str) -> dict:
-    """
-    Считывает json-файл в dict
-    :param filename: имя json-файла
-    :return: считанный словарь
-    """
-
-    with open(file=Path(Path.cwd() / filename), mode='r', encoding='utf-8') as file:
-        return dict(json.load(file))
-
-
-def calculate_word_value(word: str, json_filename: str) -> int:
-    """
-    Считает ценность слова
-    :param word: слово в виде строки
-    :param json_filename: имя json-словаря
-    :return: ценность слова
-    """
-
     word_dict = read_json(json_filename)
     word = word.lower()  # lowercase
     value = 0
@@ -150,22 +142,26 @@ def calculate_word_value(word: str, json_filename: str) -> int:
     return value
 
 
-if __name__ == "__main__":
-    print(calculate_word_value("собака", "letters_values.json"))
-    print(mark_blocked_cells([
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', 'т', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', 'о', '', '', '', '', ''],
-        ['', '', '', 'п', 'о', 'c', 'е', 'л', 'о', 'к', '', '', '', '', ''],
-        ['', '', '', 'а', '', 'а', '', '', '', '', '', 'р', '', '', ''],
-        ['', '', '', 'п', '', 'д', 'о', 'м', '', 'я', '', 'е', '', '', ''],
-        ['', '', '', 'а', '', '', '', 'а', 'з', 'б', 'у', 'к', 'а', '', ''],
-        ['', '', '', '', '', 'с', 'о', 'м', '', 'л', '', 'а', '', '', ''],
-        ['', '', '', 'я', 'м', 'а', '', 'а', '', 'о', '', '', '', '', ''],
-        ['', '', '', '', '', 'л', '', '', '', 'к', 'и', 'т', '', '', ''],
-        ['', '', '', '', 'с', 'о', 'л', 'ь', '', 'о', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-        ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-    ]))
+# if __name__ == "__main__":
+    # print(calculate_word_value("собака", "letters_values.json"))
+
+    # test_board = mark_blocked_cells([
+    #     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', 'т', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', 'о', '', '', '', '', ''],
+    #     ['', '', '', 'п', 'о', 'c', 'е', 'л', 'о', 'к', '', '', '', '', ''],
+    #     ['', '', '', 'а', '', 'а', '', '', '', '', '', 'р', '', '', ''],
+    #     ['', '', '', 'п', '', 'д', 'о', 'м', '', 'я', '', 'е', '', '', ''],
+    #     ['', '', '', 'а', '', '', '', 'а', 'з', 'б', 'у', 'к', 'а', '', ''],
+    #     ['', '', '', '', '', 'с', 'о', 'м', '', 'л', '', 'а', '', '', ''],
+    #     ['', '', '', 'я', 'м', 'а', '', 'а', '', 'о', '', '', '', '', ''],
+    #     ['', '', '', '', '', 'л', '', '', '', 'к', 'и', 'т', '', '', ''],
+    #     ['', '', '', '', 'с', 'о', 'л', 'ь', '', 'о', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    #     ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    # ])
+    #
+    # for s in test_board:
+    #     print(s)
--- scan.py
+++ scan.py
@@ -1,52 +1,68 @@
 from pyimagesearch.transform import four_point_transform
 import cv2
 import imutils
+import numpy
 
-# load the image and compute the ratio of the old height
-# to the new height, clone it, and resize it
-image = cv2.imread("images/receipt.jpg")
-ratio = image.shape[0] / 750.0
-orig = image.copy()
-image = imutils.resize(image, height=750)
 
-# grayscale, gaussian blur and edged
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 25, 170)
+def cut_by_external_contour(path: str) -> numpy.ndarray:
+    """
+    Обрезает внешний контур объекта на изображении
+    Подходит для игральной доски
+    :param path: путь к изображению
+    :return: обрезанное изображение
+    """
 
-# show the original image and the edge detected image
-cv2.imshow("Image", image)
-cv2.imshow("Edged", edged)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    image = cv2.imread(path)
+    ratio = image.shape[0] / 750.0
+    orig = image.copy()
+    image = imutils.resize(image, height=750)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # черно-белое изображение
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)  # размытие по Гауссу, оптимальные параметры: (5, 5)
+    edged = cv2.Canny(gray, 75, 150)  # изображение с границами
 
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-contours = imutils.grab_contours(contours)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    # получение некого параметра для морфологического преобразования
+    # оптимальные параметры: (7, 7)
+    # затем само морфологическое преобразование (закрытие контуров)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    edged = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
-screenCnt = None
-# loop over the contours
-for c in contours:
-    # approximation
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+    # cv2.imshow("Edged", edged)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
-    if len(approx) == 4:
-        screenCnt = approx
-        break
+    contours = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)  # массив всех контуров
+    contours = imutils.grab_contours(contours)  # ?
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]  # сортировка контуров по убыванию площади
 
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-cv2.imshow("Outline", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    screen_cnt = None  # самый большой контур с 4 точками
+    # идем по циклу контуров от самого большого до самого маленького (они уже отсортированы)
+    for c in contours:
+        peri = cv2.arcLength(c, True)  # периметр контура
 
-# apply the four point transform to obtain a top-down
-# view of the original image
-warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+        # аппроксимация: если точки слишком рядом - сливаем их в одну
+        # это позволяет выдержать небольшую погрешность на изображении
+        # оптимальный параметр от 0.02 * периметр до 0.06 * периметр
+        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
 
-# show the original and scanned images
-cv2.imshow("Original", imutils.resize(orig, height=750))
-cv2.imshow("Scanned", imutils.resize(warped, height=750))
-cv2.waitKey(0)
+        # просто берем самый большой контур с 4 точками
+        if len(approx) == 4:
+            screen_cnt = approx
+            break
+
+    # cv2.drawContours(image, [screen_cnt], -1, (0, 255, 0), 2)
+    # cv2.imshow("Outline", image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    warped = four_point_transform(orig, screen_cnt.reshape(4, 2) * ratio)  # меняем перспективу на вид сверху
+
+    # cv2.imshow("Scanned", imutils.resize(warped, height=750))
+    # cv2.waitKey(0)
+
+    return warped
+
+
+if __name__ == "__main__":
+    cv2.imshow("External contour", imutils.resize(cut_by_external_contour("images/scrabble_board1.jpg"), height=750))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
