69f56210054e02df56a27c011066c866b2e9c17e
Pavel Chernov <64083367+RiordanFols@users.noreply.github.com>
2020-05-05 17:12:05 +0300

    Update scrabble_assistant
    scrabble_assistant:
        - get_mark_row
       + get_mark_rows(big changes)
    + board_values.json

--- scrabble_assistant.py
+++ scrabble_assistant.py
@@ -13,31 +13,74 @@ def get_best_hint(board: [[str]], letters: dict) -> [[str]]:
     """
 
 
-def get_marked_row(board: [[str]], index: int) -> [str]:
+def get_marked_rows(board: [[str]]) -> [[str]]:
     """
-    Извлекает i-тый вектор из доски, помечая заблокированные клетки знаком #
+    Меняет доску, помечая заблокированные клетки знаком #
     Если у клетки есть символы сверху или снизу, то клетка заблокирована
+    Постобработка:
+    Между двумя # пустое пространство - все клетки между ними #
+    От начала до # пустое пространство - все клетки между ними #
+    От # до конца пустое пространство - все клетки между ними #
     :param board: символьный двумерный массив доски
-    :param index: индекс строки в массиве
     :return: одномерный массив символов(строка) с заблокированными клетками
     """
 
-    row = board[index].copy()  # i-тая строка доски
-    for i in range(len(row)):
-        up_block = False  # заблокировано ли сверху
-        down_block = False  # заблокировано ли снизу
+    marked_board = []  # новая доска с метками
+    for index in range(len(board)):  # идем по строкам
+        row = board[index].copy()  # i-тая строка доски
+        for symbol_index in range(len(row)):
+            up_block = False  # заблокировано ли сверху
+            down_block = False  # заблокировано ли снизу
 
-        if not row[i]:  # если в клетке пусто
-            if index > 0:
-                if board[index - 1][i]:
-                    up_block = True
-            if index < 14:
-                if board[index + 1][i]:
-                    down_block = True
-            if up_block or down_block:
-                row[i] = '#'
+            if not row[symbol_index]:  # если в клетке пусто
+                if index > 0:  # если не самая верхняя строка
+                    if board[index - 1][symbol_index]:  # если сверху буква
+                        up_block = True
+                if index < (len(board) - 1):  # если не самая нижняя строка
+                    if board[index + 1][symbol_index]:  # если снизу буква
+                        down_block = True
+                if up_block or down_block:
+                    row[symbol_index] = '#'
 
-    return row
+        # постобработка:
+        # между двумя # пустое пространство - все клетки между ними #
+        # от начала до # пустое пространство - все клетки между ними #
+        # от # до конца пустое пространство - все клетки между ними #
+
+        last_sharp_index = -2  # индекс последнего символа #
+        # -2: символы # не встречались
+        # -1: символы # встречались, но после них шли буквы
+
+        for row_index in range(len(row)):  # идем по строкам
+            if row[row_index] == '#':
+                # если уже встречали #, просто перезаписываем
+                if last_sharp_index == -1:
+                    last_sharp_index = row_index
+                # если это первая # и до этого символов не было, то все
+                # клетки до этого места помечаем #
+                elif last_sharp_index == -2:
+                    for j in range(row_index):
+                        row[j] = '#'
+                    last_sharp_index = row_index
+                # если уже встречали # и до между текущим # и прошлым не было
+                # символов, помечаем # все клетки между ними
+                else:
+                    for j in range(last_sharp_index + 1, row_index):
+                        row[j] = '#'
+                    last_sharp_index = row_index
+            # если нашли какой-то символ, сбрасываем счетчик
+            elif row[row_index] != '':
+                last_sharp_index = -1
+
+            # если дошли до конца и между последним # и концом нет символов,
+            # все клетки между ними помечаем #
+            if row_index == (len(row) - 1) and last_sharp_index != -1:
+                for j in range(last_sharp_index + 1, row_index + 1):
+                    row[j] = '#'
+
+        marked_board.append(row)  # добавляем строку к новой доске
+
+    return marked_board
 
 
 def transpose_board(board: [[str]]) -> [[str]]:
@@ -47,7 +90,7 @@ def transpose_board(board: [[str]]) -> [[str]]:
     :return: транспонированная матрица
     """
 
-    return np.array(board).transpose()
+    return list(np.array(board).transpose())
 
 
 def is_word_fits(vector: [str], word: str, start_position: int, end_position: int) -> bool:
@@ -91,23 +134,6 @@ def calculate_word_value(word: str, json_filename: str, cell_bonuses_filepath: s
     # 3 - х2 за слово
     # 4 - х3 за слово
     # 5 - стартовое поле
-
-    board_values = [[4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4],
-                    [0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0],
-                    [0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0],
-                    [1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1],
-                    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0],
-                    [0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0],
-                    [0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
-                    [4, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 4],  # центр
-                    [0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
-                    [0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 2, 0],
-                    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0],
-                    [1, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 1],
-                    [0, 0, 3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 3, 0, 0],
-                    [0, 3, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 3, 0],
-                    [4, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 4],
-                    ]
 
     letter_values = read_json(json_filename)
     word = word.lower()  # перевод в нижний регистр
@@ -174,7 +200,9 @@ if __name__ == "__main__":
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
     ]
+    # test_board = transpose_board(test_board)
 
-    for j in range(14):
-        print(get_marked_row(test_board, j))
-    # print(get_marked_vector(test_board, 6))
+    test_marked_board = get_marked_rows(test_board)
+    for i in range(len(test_marked_board)):
+        print(test_marked_board[i])
+    # print(get_marked_row(test_board, 12))
