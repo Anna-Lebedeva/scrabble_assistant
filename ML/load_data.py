6c0972604b0cc26e99348cce03106b12e79f0691
ex10se <pyth0n@inbox.ru>
2020-05-20 13:58:12 +0300

    Add ML files

---
+++ ML/load_data.py
@@ -0,0 +1,39 @@
+import pickle
+from scipy import misc
+import numpy as np
+import os
+import imageio
+import tensorflow as tf
+
+path_to_classifier = "int_to_word_out.pickle"
+path_to_dataset = "dataset_image/"
+
+label = os.listdir(path_to_dataset)
+dataset = []
+for image_label in label:
+
+    images = os.listdir(path_to_dataset + image_label)
+
+    for image in images:
+        img = imageio.imread(path_to_dataset + image_label + "/" + image)
+        img = tf.image.resize(img, (64, 64))
+        dataset.append((img, image_label))
+
+X = []
+Y = []
+
+for input, image_label in dataset:
+    X.append(input)
+
+    Y.append(label.index(image_label))
+
+X = np.array(X)
+Y = np.array(Y)
+
+X_train, y_train, = X, Y
+
+data_set = (X_train, y_train)
+
+save_label = open(path_to_classifier, "wb")
+pickle.dump(label, save_label)
+save_label.close()
---
+++ ML/mishiny_scripty/!Вырезалка букв.py
@@ -0,0 +1,66 @@
+import fnmatch
+import os
+import shutil
+
+import cv2
+import imutils
+
+from scan import cut_board_on_cells
+from scan import cut_by_external_contour
+from scan import cut_by_internal_contour
+
+path_to_raw = "../!raw_images_to_cut/"
+path_to_train = "../dataset_image/"
+
+str1 = [1, 16, 0]  # Начало, конец+1, номер строки, номер столбца
+str2 = [16, 31, 1]
+str3 = [31, 33, 2]
+asterisk = [33, 34, 2, 2]
+red = [91, 92, 7, 0]
+blue = [48, 49, 3, 3]
+green = [46, 47, 3, 0]
+white = [98, 99, 7, 7]
+yellow = [77, 78, 5, 1]
+empty = [78, 79, 5]
+all_types = [str1, str2, str3, asterisk, red, blue, green, white, yellow, empty]
+
+for a in range(len(all_types)):
+    current = all_types[a]
+
+    for bukva in range(current[0], current[1]):
+
+        if current == asterisk:
+            b = "Asterisk"
+            x = current[3]
+        elif current == red:
+            b = "Red"
+            x = current[3]
+        elif current == blue:
+            b = "Blue"
+            x = current[3]
+        elif current == green:
+            b = "Green"
+            x = current[3]
+        elif current == white:
+            b = "White"
+            x = current[3]
+        elif current == yellow:
+            b = "Yellow"
+            x = current[3]
+        elif current == empty:
+            b = "Empty"
+            x = bukva - current[0] + 2
+        else:
+            b = bukva
+            x = bukva - current[0]
+
+        shutil.rmtree(path_to_train + str(b), ignore_errors=True)
+        os.mkdir(path_to_train + str(b))
+
+        for i in range(1, len((os.listdir(path_to_raw)))+1):
+            warped = cut_by_external_contour(path_to_raw + str(i) + ".jpg")
+            warped = imutils.resize(cut_by_internal_contour(warped, left=3.3, top=3.0, right=0.3, bot=1.4), height=750)
+            squares_array = cut_board_on_cells(warped)
+
+            cv2.imwrite(path_to_train + str(b) + "/" + str(len(os.listdir(path_to_train + str(b) + "")) + 1) + ".jpg", squares_array[current[2]][x])
+            print(str(b) + ": " + str(i) + "/" + str(len(os.listdir(path_to_raw))) + " :" + str(round(i*100/len(os.listdir(path_to_raw)), 1)) + "%")
